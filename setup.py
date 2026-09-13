2996b9d94569b5c95bb504dedd5afc172f2f5d38
Amar Nath <chaurasiyaamarnath96@gmail.com>
2026-09-13 11:45:21 +0530

    requirements.txt installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="garbage_classification",
+    version="0.1.0",
+    author="Amar Nath Chaurasiya",
+    author_email="chaurasiyaamarnath96@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
